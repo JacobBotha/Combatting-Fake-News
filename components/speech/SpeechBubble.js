b35760a20f49278c52ae0393c2422a31ab640433
Ben Thorpe <ben.thorpy@gmail.com>
2021-10-18 13:20:52 +1000

    Added story images, created title page, updated SpeechBubble component

--- components/speech/SpeechBubble.js
+++ components/speech/SpeechBubble.js
@@ -16,25 +16,25 @@ const SpeechBox = styled.div`
   flex: 1;
   margin: 0 0;
   padding: 10px;
-  background: #9dcae6;
+  background: ${props => (props.alt ? "#9dcae6" : "#8EB2E7") };
   font-family: Baloo Tamma;
   font-style: normal;
   font-weight: normal;
-  font-size: 38px;
-  line-height: 65px;
+  font-size: 28px;
+  line-height: 38px;
   color: #FFFFFF;
   border-radius: 10px;
 `;
 
 const SpeechArrowLeft = styled.div`
   border-top: 10px solid transparent;
-  border-right: 20px solid #9dcae6;
+  border-right: 20px solid ${props => (props.alt ? "#9dcae6" : "#8EB2E7") };
   border-bottom: 10px solid transparent;
 `;
 
 const SpeechArrowRight = styled.div`
   border-top: 10px solid transparent;
-  border-left: 20px solid #9dcae6;
+  border-left: 20px solid ${props => (props.alt ? "#9dcae6" : "#8EB2E7") };
   border-bottom: 10px solid transparent;
 `;
 
@@ -42,7 +42,7 @@ const SpeechArrowBottomL = styled.div`
   align-self: flex-start;
   margin-left: 10px;
   border-left: 10px solid transparent;
-  border-top: 20px solid #9dcae6;
+  border-top: 20px solid ${props => (props.alt ? "#9dcae6" : "#8EB2E7") };
   border-right: 10px solid transparent;
 `;
 
@@ -50,37 +50,37 @@ const SpeechArrowBottomR = styled.div`
   align-self: flex-end;
   margin-right: 10px;
   border-left: 10px solid transparent;
-  border-top: 20px solid #9dcae6;
+  border-top: 20px solid ${props => (props.alt ? "#9dcae6" : "#8EB2E7") };
   border-right: 10px solid transparent;
 `;
 
-const SpeechBubble = ({ type, children }) => {
+const SpeechBubble = ({ alt, type, children }) => {
   if (type == "right") {
     return (
       <SpeechBubbleContainerH>
-        <SpeechArrowLeft />
-        <SpeechBox>{children}</SpeechBox>
+        <SpeechArrowLeft alt={alt}/>
+        <SpeechBox alt={alt}>{children}</SpeechBox>
       </SpeechBubbleContainerH>
     );
   } else if (type == "left") {
     return (
       <SpeechBubbleContainerH>
-        <SpeechBox>{children}</SpeechBox>
-        <SpeechArrowRight />
+        <SpeechBox alt={alt}>{children}</SpeechBox>
+        <SpeechArrowRight alt={alt}/>
       </SpeechBubbleContainerH>
     );
   } else if (type == "bottomleft") {
     return (
       <SpeechBubbleContainerV>
-        <SpeechBox>{children}</SpeechBox>
-        <SpeechArrowBottomL />
+        <SpeechBox alt={alt}>{children}</SpeechBox>
+        <SpeechArrowBottomL alt={alt}/>
       </SpeechBubbleContainerV>
     );
   } else {
     return (
       <SpeechBubbleContainerV>
-        <SpeechBox>{children}</SpeechBox>
-        <SpeechArrowBottomR />
+        <SpeechBox alt={alt}>{children}</SpeechBox>
+        <SpeechArrowBottomR alt={alt}/>
       </SpeechBubbleContainerV>
     );
   }
